2c1228a67aa51039e8209c1953fc1d79d4ae1456
Nate McDowell <nateclaudemcdowel@gmail.com>
2019-03-14 17:01:58 -0400

    Use date

--- create-release.js
+++ create-release.js
@@ -1,4 +1,3 @@
-const HEADERS = {headers: {'User-Agent': 'request'}};
 const TRAVIS_BRANCH_OR_TAG = process.argv[2];
 const GH_TOKEN = process.argv[3];
 const OWNER = process.argv[4];
@@ -7,12 +6,13 @@ const TOKEN = '?access_token=' + GH_TOKEN;
 
 let HttpWrapper = require('./http-wrapper.js');
 let http = new HttpWrapper();
+let date = Date.now();
 
 class GithubReleaseCreator {
   
   getReleaseData() {
     return JSON.stringify({
-      tag_name: 'release-' + TRAVIS_BRANCH_OR_TAG,
+      tag_name: 'release-' + TRAVIS_BRANCH_OR_TAG + '-' + date,
       target_commitish: TRAVIS_BRANCH_OR_TAG
     });
   }
@@ -26,7 +26,7 @@ class GithubReleaseCreator {
       'https://uploads.github.com/repos/' + OWNER + '/' + REPO + '/releases/' + id + '/assets' + TOKEN + '&name=release.tar.gz'
     ).then(res => {
 
-      console.log(res);
+      console.log(res.body);
       console.log('Release successfully uploaded...')
       process.exit(0);
 
@@ -38,64 +38,20 @@ class GithubReleaseCreator {
     });
   }
   
-  getReleases() {
-    http.makeGetRequest(
-      'https://api.github.com/repos/' + OWNER + '/' + REPO + '/releases' + TOKEN,
-      HEADERS
-    ).then(res => {
-      console.log(TRAVIS_BRANCH_OR_TAG);
-      const releaseFound = JSON.parse(res.body).find(release => release.tag_name === ('release-' + TRAVIS_BRANCH_OR_TAG));
-      if (releaseFound && releaseFound.assets && releaseFound.assets.length) {
-        
-        console.log('release found')
-        http.makeDeleteRequest(
-          'https://api.github.com/repos/' + OWNER + '/' + REPO + '/releases/assets/' + releaseFound.assets[0].id + TOKEN
-        ).then(res => {
-          this.handleRelease(releaseFound);
-        });
-  
-      } else {
-  
-        this.handleRelease(releaseFound);
-  
-      }
-    });
-  }
-  
-  handleRelease(releaseFound) {
-    if (!releaseFound) {
-  
-      this.createRelease();
-  
-    } else {
-  
-      this.editRelease(releaseFound.id);
-  
-    }
-  }
-  
   createRelease() {
+
     http.makePostRequest(
       'https://api.github.com/repos/' + OWNER + '/' + REPO + '/releases' + TOKEN,
       this.getReleaseData()
     ).then(res => {
-      console.log(res);
+
+      console.log(res.body);
       this.uploadReleaseZip(JSON.parse(res.body).id);
   
     });
   }
-  
-  editRelease(id) {
-    http.makePatchRequest(
-      'https://api.github.com/repos/' + OWNER + '/' + REPO + '/releases/' + id + TOKEN,
-      this.getReleaseData()
-    ).then(() => {
-  
-      this.uploadReleaseZip(id);
-  
-    });
-  }
+
 }
 
 let creator = new GithubReleaseCreator();
-creator.getReleases();
+creator.createRelease()
---
+++ download-release.js
@@ -0,0 +1,63 @@
+const HEADERS = {headers: {'User-Agent': 'request'}};
+const TRAVIS_BRANCH_OR_TAG = process.argv[2];
+const GH_TOKEN = process.argv[3];
+const OWNER = process.argv[4];
+const REPO = process.argv[5];
+const TOKEN = '?access_token=' + GH_TOKEN;
+let HttpWrapper = require('./http-wrapper.js');
+let _ = require('lodash');
+let shell = require('shelljs');
+let http = new HttpWrapper();
+let date = Date.now();
+
+class GithubReleaseDownloader {
+
+  downloadRelease() {
+    this.getReleases();
+  }
+  
+  getReleaseData() {
+    return JSON.stringify({
+      tag_name: 'release-' + TRAVIS_BRANCH_OR_TAG + '-' + date,
+      target_commitish: TRAVIS_BRANCH_OR_TAG
+    });
+  }
+  
+  getReleases() {
+    http.makeGetRequest(
+      'https://api.github.com/repos/' + OWNER + '/' + REPO + '/releases' + TOKEN,
+      HEADERS
+    ).then(res => {
+
+      const releasesFound = JSON.parse(res.body).filter(release => release.tag_name.indexOf( 'release-' + TRAVIS_BRANCH_OR_TAG ) > -1);
+      const releasesSorted = _.sortBy(releasesFound, release => release.created_at);
+      const lastRelease = releasesSorted[ releasesSorted.length -1 ];
+
+      console.log('Using asset id: ' + lastRelease.assets[0].id);
+      console.log('Release created at: ' + lastRelease.created_at);
+
+      this.download(releasesSorted[0].assets[0].id);
+      this.makeReleaseDir();
+      this.unzipRelease();
+
+    });
+  }
+
+  download(assetId) {
+    shell.exec(
+      `wget -q --auth-no-challenge --header='Accept:application/octet-stream' https://api.github.com/repos/${OWNER}/${REPO}/releases/assets/${assetId}?access_token=${GH_TOKEN} -O release.tar.gz`
+    )
+  }
+
+  makeReleaseDir() {
+    shell.exec('mkdir release');
+  }
+
+  unzipRelease() {
+    shell.exec('tar -zvxf release.tar.gz');
+  }
+
+}
+
+let downloader = new GithubReleaseDownloader();
+downloader.downloadRelease();
